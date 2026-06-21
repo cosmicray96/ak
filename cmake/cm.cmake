b785aca9df641a86a19b2f2db188ccad6d41390b
cosmicray96 <cosmicrayleigh02@gmail.com>
2026-06-21 16:48:45 +0000

    simpler cmake

--- cmake/cm.cmake
+++ cmake/cm.cmake
@@ -1,18 +1,4 @@
-message("Cmake Template. Language: C, Version: 11")
+message("Cmake Template. Language: C, Version: 12")
 
 include("${CMAKE_CURRENT_LIST_DIR}/config.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/target.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/utils.cmake")
-
-macro(cm_begin)
-	cmi_proj_root_dir_set()
-	cmi_check_build_type()
-	cmi_c_set()
-endmacro()
-
-
-macro(cm_end)
-	cmi_output_dirs_set()
-	cmi_clangd_commands_json()
-endmacro()
-
--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,24 +1,19 @@
 #--- c ---#
-macro(cmi_c_set)
+macro(cm_c_set)
 	set(CMAKE_C_STANDARD "99")
 	set(CMAKE_C_STANDARD_REQUIRED ON)
 	set(CMAKE_C_EXTENSIONS OFF)
 	set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 	set(CMAKE_C_VISIBILITY_PRESET hidden)
 	set(CMAKE_VISIBILITY_INLINES_HIDDEN 1)
+
+	#	set(CMAKE_INSTALL_RPATH "\$ORIGIN")
+	#set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+	#set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+	#	add_link_options("-Wl,-rpath,\$ORIGIN")
+
 	enable_testing()
-endmacro()
-
-#--- proj root ---#
-set(cm_proj_root_dir "")
-macro(cmi_proj_root_dir_set)
-	get_filename_component(cm_proj_root_dir "${CMAKE_SOURCE_DIR}" ABSOLUTE)
-endmacro()
-
-#--- output dir name ---#
-set(cm_output_dir_suffix "")
-macro(cm_output_dir_suffix_set name)
-	set(cm_output_dir_suffix  ${name})
 endmacro()
 
 #--- dirs ---#
@@ -26,12 +21,10 @@ set(cm_bin_dir "")
 set(cm_junk_dir "")
 set(cm_tests_dir "")
 set(cm_exe_dir "")
-macro(cmi_output_dirs_set)
-
-	set(suffix ${cm_output_dir_suffix})
+macro(cm_output_dirs_set suffix)
 
 	set(cm_bin_dir
-		"${cm_proj_root_dir}/_bin/${CMAKE_BUILD_TYPE}_${suffix}"
+		"${CMAKE_SOURCE_DIR}/_bin/${CMAKE_BUILD_TYPE}_${suffix}"
 	)
 	set(cm_junk_dir "${cm_bin_dir}/_junk")
 	set(cm_tests_dir "${cm_bin_dir}/_tests")
@@ -43,12 +36,12 @@ macro(cmi_output_dirs_set)
 endmacro()
 
 #--- clangd ---#
-macro(cmi_clangd_commands_json)
+macro(cm_clangd_commands_json)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 add_custom_target(copy_compile_commands ALL
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${CMAKE_BINARY_DIR}/compile_commands.json
-        ${proj_root_dir}/compile_commands.json
+				${CMAKE_SOURCE_DIR}/compile_commands.json
     COMMENT "Copying compile_commands.json to project root"
     VERBATIM
 )
